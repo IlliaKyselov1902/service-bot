ba7d587a8435229bd7eef63a121138ce3268ff85
IlliaKyselov1902 <qwertytwenty129@gmail.com>
2023-11-06 23:32:54 +0200

    Set up bot polling

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
+import os
+import logging
+import asyncio
+
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher
+
+
+# Loading data from the .env file
+load_dotenv()
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+
+# Set up bot and dispatcher
+bot = Bot(token=os.environ.get('TOKEN'))
+dp = Dispatcher()
+
+
+# Define main function
+async def main():
+    await dp.start_polling(bot)
+
 if __name__ == '__main__':
-    print('none')
+    asyncio.run(main())
